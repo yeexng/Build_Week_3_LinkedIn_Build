04484db4d2e2a054526945984a3411f687627755
Batı Gökovalı <batigokovali@icloud.com>
2023-02-20 12:35:29 +0300

    Added redux store for future

---
+++ src/redux/store/index.js
@@ -0,0 +1,34 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
+import localStorage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    storage: localStorage,
+    key: 'root', // this brings the whole redux store into persistency
+    transforms: [
+        encryptTransform({
+            secretKey: process.env.REACT_APP_SECRET_KEY
+        }),
+    ],
+}
+
+const combinedReducer = combineReducers({
+
+})
+
+const persistedReducer = persistReducer(persistConfig, combinedReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware({
+            serializableCheck: false,
+        })
+    },
+})
+
+const persistedStore = persistStore(store)
+
+export { store, persistedStore }
