680ceb5089b1b3c514dbd6bb46a642f61ebf38d4
yeexng <118996751+yeexng@users.noreply.github.com>
2023-02-20 15:54:23 +0000

    Modal box (profile)  done

--- src/components/DropUpChat.jsx
+++ src/components/DropUpChat.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { MDBCollapse, MDBListGroup, MDBListGroupItem } from "mdb-react-ui-kit";
+import {
+  MDBCollapse,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBInput,
+} from "mdb-react-ui-kit";
 import { BsThreeDots } from "react-icons/bs";
 import { FiEdit } from "react-icons/fi";
 import { RiArrowDownSLine } from "react-icons/ri";
 import { RiArrowUpSLine } from "react-icons/ri";
+import { BiSearch } from "react-icons/bi";
+import { Image } from "react-bootstrap";
 
 const DropUpChat = () => {
   const [showShow, setShowShow] = useState(false);
@@ -29,11 +36,50 @@ const DropUpChat = () => {
           </div>
           <MDBCollapse show={showShow}>
             <MDBListGroup light>
-              <MDBListGroupItem className="px-3">Bati</MDBListGroupItem>
-              <MDBListGroupItem className="px-3">Piotr</MDBListGroupItem>
-              <MDBListGroupItem className="px-3">May</MDBListGroupItem>
-              <MDBListGroupItem className="px-3">Lux</MDBListGroupItem>
-              <MDBListGroupItem className="px-3">Riven</MDBListGroupItem>
+              <MDBListGroupItem className="px-1">
+                <MDBInput label="Search" id="typeText" type="text" />
+              </MDBListGroupItem>
+
+              <MDBListGroupItem className="px-3">
+                <Image
+                  className="chat-avatar"
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                  roundedCircle
+                />{" "}
+                Bati
+              </MDBListGroupItem>
+              <MDBListGroupItem className="px-3">
+                <Image
+                  className="chat-avatar"
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                  roundedCircle
+                />{" "}
+                Piotr
+              </MDBListGroupItem>
+              <MDBListGroupItem className="px-3">
+                <Image
+                  className="chat-avatar"
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                  roundedCircle
+                />{" "}
+                May
+              </MDBListGroupItem>
+              <MDBListGroupItem className="px-3">
+                <Image
+                  className="chat-avatar"
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                  roundedCircle
+                />{" "}
+                Lux
+              </MDBListGroupItem>
+              <MDBListGroupItem className="px-3">
+                <Image
+                  className="chat-avatar"
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                  roundedCircle
+                />{" "}
+                Riven
+              </MDBListGroupItem>
             </MDBListGroup>
           </MDBCollapse>
         </div>
--- src/components/ProfileAvatar.jsx
+++ src/components/ProfileAvatar.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Container, Button, Image, Row, Col, Modal } from "react-bootstrap";
 import { AiFillCamera } from "react-icons/ai";
 import { FiEdit2 } from "react-icons/fi";
+import { RiGalleryFill } from "react-icons/ri";
+import { ImBin } from "react-icons/im";
 
 const ProfileAvatar = () => {
   const [show, setShow] = useState(false);
@@ -53,21 +55,60 @@ const ProfileAvatar = () => {
             </Modal.Footer>
           </Modal>
 
-          <Modal show={showPic} onHide={handleClosePic} animation={false}>
-            <Modal.Header closeButton>
-              <Modal.Title>Profile Editing</Modal.Title>
+          <Modal
+            show={showPic}
+            onHide={handleClosePic}
+            animation={false}
+            className="modal-profile"
+          >
+            <Modal.Header closeButton className="modal-wrapper">
+              <Modal.Title>Profile photo</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-              Woohoo, you're reading this text in a modal!
+            <div className="d-flex justify-content-center modal-wrapper">
+              <Image
+                className="img-fluid model-profile-pic"
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                roundedCircle
+              />
+            </div>
+            <div className="modal-wrapper">
+              <div
+                className="btn btn-outline-light"
+                style={{ borderRadius: 30 }}
+              >
+                <p className="text-light">Anyone</p>
+              </div>
+            </div>
+            <Modal.Body className="modal-wrapper">
+              <Row className="justify-content-between">
+                <Col md={8}>
+                  <div className="btn modal-btn">
+                    <FiEdit2 className="text-light" />
+                    <br></br>
+                    <p className="text-light">Edit</p>
+                  </div>
+                  <div className="btn modal-btn">
+                    <AiFillCamera className="text-light" />
+                    <br></br>
+                    <p className="text-light">Add photo</p>
+                  </div>
+                  <div className="btn modal-btn">
+                    <RiGalleryFill className="text-light" />
+                    <br></br>
+                    <p className="text-light">Frames</p>
+                  </div>
+                </Col>
+                <Col md={3}>
+                  <div>
+                    <div className="btn modal-btn">
+                      <ImBin className="text-light" />
+                      <br></br>
+                      <p className="text-light">Delete</p>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
             </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClosePic}>
-                Close
-              </Button>
-              <Button variant="primary" onClick={handleClosePic}>
-                Save Changes
-              </Button>
-            </Modal.Footer>
           </Modal>
 
           <Row className="details-box justify-content-between">
