7801542378670e24917593ae5135dd422251cffc
yeexng <118996751+yeexng@users.noreply.github.com>
2023-02-20 12:22:42 +0000

    Footer Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import ProfileAvatar from "./components/ProfileAvatar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <ProfileAvatar />
+      <Footer />
     </div>
   );
 }
