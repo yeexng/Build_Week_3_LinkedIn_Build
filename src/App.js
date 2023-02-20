13854cdd33c35ce3a7ba97e1d7eba9c9c2626690
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-02-20 16:39:12 +0100

    Navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+    </div>
+  );
 }
 
 export default App;
