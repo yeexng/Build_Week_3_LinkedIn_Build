dd8dd46f853dbd5b6e66247363ec327e3b669edc
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-02-21 12:19:59 +0100

    changed the padding up

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,33 +3,32 @@ import LeftSide from "./LeftSide";
 import RightSide from "./RightSide";
 import Footer from "./Footer";
 import DropUpChat from "./DropUpChat";
-import NavBar from './NavBar'
-
+import NavBar from "./NavBar";
 
 const MainLayout = () => {
-    return (
-        <>
-            <NavBar></NavBar>
-            <Container>
-                <Row className="mt-3">
-                    <Col lg={9}>
-                        <LeftSide></LeftSide>
-                    </Col>
-                    <Col lg={3}>
-                        <RightSide></RightSide>
-                    </Col>
-                </Row>
-                <Row>
-                    <Footer></Footer>
-                </Row>
-                <Row>
-                    <Col lg={4}>
-                        <DropUpChat></DropUpChat>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <NavBar></NavBar>
+      <Container>
+        <Row className="mt-5 pt-5">
+          <Col lg={9}>
+            <LeftSide></LeftSide>
+          </Col>
+          <Col lg={3}>
+            <RightSide></RightSide>
+          </Col>
+        </Row>
+        <Row>
+          <Footer></Footer>
+        </Row>
+        <Row>
+          <Col lg={4}>
+            <DropUpChat></DropUpChat>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-export default MainLayout
+export default MainLayout;
