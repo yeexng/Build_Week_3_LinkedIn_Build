df275e6d632a0d1e35e9f94135865d6ac1264ba9
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-02-21 12:04:10 +0100

    search array

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Navbar,
   Nav,
@@ -7,9 +8,28 @@ import {
   NavDropdown,
   Button,
 } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllProfileActionAsync,
+  getSearchResultActionAsync,
+} from "../redux/actions";
 import "../styles/navbar.css";
 
 const NavBar = () => {
+  const [searchValue, getSearchValue] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProfileActionAsync());
+  }, []);
+
+  let allProfiles = useSelector((state) => state.profile.content);
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(allProfiles[0]);
+    dispatch(getSearchResultActionAsync(allProfiles[0], searchValue));
+  };
+
   let modal = document.getElementById("myModal");
   window.onclick = function (event) {
     if (event.target === modal) {
@@ -18,7 +38,6 @@ const NavBar = () => {
   };
   const headerChange = function () {
     if (window.pageYOffset > 500) {
-      console.log("it went there");
       document.querySelector(".second-nav").classList.add("scrolled-bg");
       document.querySelector(".second-nav").classList.remove("back-bg");
     } else if (window.pageYOffset < 500) {
@@ -46,17 +65,20 @@ const NavBar = () => {
               <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
             </svg>
           </Navbar.Brand>
-          <Form inline id="search-form-wrapper">
+          <Form inline id="search-form-wrapper" onSubmit={handleChange}>
             <i className="bi bi-search"></i>
             <FormControl
               type="text"
               placeholder="Search"
               className="mr-sm-2 show-search"
+              value={searchValue}
+              onChange={(e) => getSearchValue(e.target.value)}
             />
             {/* <Button variant="outline-primary">Search</Button> */}
           </Form>
+          {/* <div id="search-popup">testing</div> */}
           <Nav className="ml-auto ">
-            <Nav.Link href="#home">
+            <Nav.Link href="#actionh">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -71,7 +93,7 @@ const NavBar = () => {
               </svg>
               <p className="text-gone">Home</p>
             </Nav.Link>
-            <Nav.Link href="#">
+            <Nav.Link href="actionmn">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -86,7 +108,7 @@ const NavBar = () => {
               </svg>{" "}
               <p className="text-gone">My Network</p>
             </Nav.Link>
-            <Nav.Link href="#">
+            <Nav.Link href="actionj">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -101,7 +123,7 @@ const NavBar = () => {
               </svg>
               <p className="text-gone">Jobs</p>
             </Nav.Link>
-            <Nav.Link href="#">
+            <Nav.Link href="actionm">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -116,7 +138,7 @@ const NavBar = () => {
               </svg>
               <p className="text-gone">Messaging</p>
             </Nav.Link>
-            <Nav.Link href="#" id="notification-parent">
+            <Nav.Link href="#actionn" id="notification-parent">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,36 +1,39 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from 'redux-persist'
-import { encryptTransform } from 'redux-persist-transform-encrypt'
-import localStorage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from "redux-persist";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import localStorage from "redux-persist/lib/storage";
+import allProfileReducer from "../reducers/allProfileReducer";
 import reducerToBeDeleted from "../reducers/reducerToBeDeleted";
-
+import searchReducer from "../reducers/searchReducer";
 
 const persistConfig = {
-    storage: localStorage,
-    key: 'root', // this brings the whole redux store into persistency
-    transforms: [
-        encryptTransform({
-            secretKey: process.env.REACT_APP_SECRET_KEY
-        }),
-    ],
-}
+  storage: localStorage,
+  key: "root", // this brings the whole redux store into persistency
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_KEY,
+    }),
+  ],
+};
 
 const combinedReducer = combineReducers({
-    willBeDeleted: reducerToBeDeleted,
-})
+  profile: allProfileReducer,
+  search: searchReducer,
+  willBeDeleted: reducerToBeDeleted,
+});
 
-const persistedReducer = persistReducer(persistConfig, combinedReducer)
+const persistedReducer = persistReducer(persistConfig, combinedReducer);
 
 const store = configureStore({
-    reducer: persistedReducer,
+  reducer: persistedReducer,
 
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware({
-            serializableCheck: false,
-        })
-    },
-})
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
+});
 
-const persistedStore = persistStore(store)
+const persistedStore = persistStore(store);
 
-export { store, persistedStore }
+export { store, persistedStore };
