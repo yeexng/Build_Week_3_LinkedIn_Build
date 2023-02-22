929c6c692a27d7f284d6c3154a19d956760ff2e8
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-02-22 11:53:18 +0100

    css newsFeed

--- src/components/NewsFeedMiddle.jsx
+++ src/components/NewsFeedMiddle.jsx
@@ -1,34 +1,117 @@
 import { useSelector } from "react-redux";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Button, Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
 const NewsFeedMiddle = () => {
   const userProfileAPIRS = useSelector((state) => state.userDataAPI.stock);
 
   return (
-    <Card>
-      <Card.Img
-        variant="top"
-        src={userProfileAPIRS && userProfileAPIRS.image}
-        alt="profile in navigation bar"
-        className="profile-photo-dropdown"
-      />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroupItem>Cras justo odio</ListGroupItem>
-        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+    <>
+      <Card id="news-feed-mid-section">
+        <div className="d-flex flex-column">
+          <div className="d-flex">
+            <img
+              src={userProfileAPIRS && userProfileAPIRS.image}
+              alt="profile"
+              className="profile-middle m-2"
+            ></img>
+            <Button className="w-100 m-3 post-button">
+              <span>Start a post</span>
+            </Button>
+          </div>
+          <div className="d-flex justify-content-around mb-3">
+            <Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="currentColor"
+                class="svg-photo"
+                width="24"
+                height="24"
+                focusable="false"
+              >
+                <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm1 13a1 1 0 01-.29.71L16 14l-2 2-6-6-4 4V7a1 1 0 011-1h14a1 1 0 011 1zm-2-7a2 2 0 11-2-2 2 2 0 012 2z"></path>
+              </svg>
+              <span>Photo</span>
+            </Button>
+            <Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="currentColor"
+                class="svg-video"
+                width="24"
+                height="24"
+                focusable="false"
+              >
+                <path d="M19 4H5a3 3 0 00-3 3v10a3 3 0 003 3h14a3 3 0 003-3V7a3 3 0 00-3-3zm-9 12V8l6 4z"></path>
+              </svg>
+              <span>Video</span>{" "}
+            </Button>
+            <Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="currentColor"
+                class="svg-event"
+                width="24"
+                height="24"
+                focusable="false"
+              >
+                <path d="M3 3v15a3 3 0 003 3h12a3 3 0 003-3V3zm13 1.75A1.25 1.25 0 1114.75 6 1.25 1.25 0 0116 4.75zm-8 0A1.25 1.25 0 116.75 6 1.25 1.25 0 018 4.75zM19 18a1 1 0 01-1 1H6a1 1 0 01-1-1V9h14zm-5.9-3a1 1 0 00-1-1H12a3.12 3.12 0 00-1 .2l-1-.2v-3h3.9v1H11v1.15a3.7 3.7 0 011.05-.15 1.89 1.89 0 012 1.78V15a1.92 1.92 0 01-1.84 2H12a1.88 1.88 0 01-2-1.75 1 1 0 010-.25h1a.89.89 0 001 1h.1a.94.94 0 001-.88z"></path>
+              </svg>
+              <span>Event</span>
+            </Button>
+            <Button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="currentColor"
+                class="svg-article"
+                width="24"
+                height="24"
+                focusable="false"
+              >
+                <path d="M21 3v2H3V3zm-6 6h6V7h-6zm0 4h6v-2h-6zm0 4h6v-2h-6zM3 21h18v-2H3zM13 7H3v10h10z"></path>
+              </svg>
+              <span>Write artice</span>
+            </Button>
+          </div>
+        </div>
+      </Card>
+      <Card id="news-feed-mid-section-lower" className="mt-3">
+        <div className="d-flex flex-column mx-2 my-2">
+          <div className="d-flex">
+            <img
+              src={userProfileAPIRS && userProfileAPIRS.image}
+              alt="profile"
+              className="profile-middle m-2"
+            ></img>
+            <div>
+              <p>
+                <strong>
+                  {userProfileAPIRS && userProfileAPIRS.name}{" "}
+                  {userProfileAPIRS && userProfileAPIRS.surname}
+                </strong>
+              </p>
+              <p>
+                <em>{userProfileAPIRS && userProfileAPIRS.title}</em>
+              </p>
+              <p>DATE:</p>
+            </div>
+          </div>
+          <div className="mx-3 my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            corrupti incidunt nihil ipsam voluptate atque? Accusamus iusto optio
+            voluptatibus consequuntur dicta fuga recusandae, labore praesentium
+            et impedit, saepe repellat officiis?
+          </div>
+        </div>
+      </Card>
+    </>
   );
 };
 
