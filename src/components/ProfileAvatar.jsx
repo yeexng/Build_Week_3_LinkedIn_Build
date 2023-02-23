83d468b1ca5471bebba2fb34e87956743633794f
Batı Gökovalı <batigokovali@icloud.com>
2023-02-23 19:46:08 +0300

    update

--- src/components/ProfileAvatar.jsx
+++ src/components/ProfileAvatar.jsx
@@ -50,25 +50,6 @@ const ProfileAvatar = () => {
 
   const inputRef = useRef(null);
 
-  const handleFileChange = event => {
-    const fileObj = event.target.files && event.target.files[0];
-    if (!fileObj) {
-      return;
-    }
-
-    console.log('fileObj is', fileObj);
-
-    // 👇️ reset file input
-    event.target.value = null;
-
-    // 👇️ is now empty
-    console.log(event.target.files);
-
-    // 👇️ can still access file object here
-    console.log(fileObj);
-    console.log(fileObj.name);
-  };
-
   const handleClick = () => {
     // 👇️ open file input box on click of other element
     inputRef.current.click();
